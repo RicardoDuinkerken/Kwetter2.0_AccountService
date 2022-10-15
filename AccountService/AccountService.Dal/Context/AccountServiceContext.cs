96e621a3cb341946a52bbb658b8534726522057f
Ricardo <Ricardoduinkerken@live.nl>
2022-10-15 17:18:50 +0200

    add account model and accountservice context to AccountService.Dal

---
+++ AccountService/AccountService.Dal/Context/AccountServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using AccountService.Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountService.Dal.Context;
+
+public class AccountServiceContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+
+    public AccountServiceContext(DbContextOptions<AccountServiceContext> options) : base(options)
+    {
+                
+    }
+}
